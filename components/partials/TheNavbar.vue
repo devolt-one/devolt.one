a25f3a28fb964fb589ba4e6b1c77769d09ccefe8
Leonid Meleshin <leon0399@explabs.ru>
2020-05-25 18:18:07 +0300

    feat(Navbar): add simple layout

---
+++ components/partials/TheNavbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="flex items-center justify-between pl-4 lg:pl-12">
+    <!-- Logo -->
+    <nuxt-link :to="{ name: 'index' }">
+      <h1 class="m-0 h-0 w-0 overflow-hidden">Devolt</h1>
+      <devolt-logo class="h-8 lg:h-12 w-auto text-black" />
+    </nuxt-link>
+
+    <div class="flex items-center">
+      <button class="p-3 lg:p-4 bg-primary-base text-white focus:outline-none">
+        <menu-icon class="h-12 w-12 lg:h-16 lg:w-16" />
+      </button>
+    </div>
+  </header>
+</template>
+
+<script>
+import DevoltLogo from '@/assets/images/logo.svg?inline'
+
+import MenuIcon from '@/assets/icons/elastofont/menu.svg?inline'
+
+export default {
+  components: {
+    DevoltLogo,
+    MenuIcon
+  }
+}
+</script>
