4e87d7b3c1fc14c7d8a8d357a0e7a50b4a72bf97
yana <yk@devolt.one>
2020-06-25 01:19:20 +0300

    feat: fix page services and added filer image

---
+++ components/partials/services/ServiceExcerpt.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="service-excerpt"><slot /></div>
+</template>
+
+<style lang="scss">
+.service-excerpt {
+  h1 {
+    @apply text-2xl leading-tight mb-6;
+  }
+
+  p,
+  ul {
+    @apply text-lg my-6;
+  }
+
+  /* purgecss ignore */
+  ul,
+  ol {
+    @screen md {
+      column-count: 2;
+    }
+
+    li {
+      @apply my-1;
+      position: relative;
+      padding-left: 2.25rem;
+      text-transform: uppercase;
+      font-weight: 600;
+
+      &::before {
+        content: ' ';
+
+        position: absolute;
+        left: 0;
+        top: 0.7rem;
+
+        width: 1.5rem;
+        height: 1px;
+
+        background-color: currentColor;
+        // transform: translate(0, -50%);
+      }
+    }
+  }
+}
+</style>
--- pages/services.vue
+++ pages/services.vue
@@ -1,125 +1,28 @@
 <template>
   <div>
     <page-heading :title="$t('service.meta.title')"></page-heading>
-
-    <section class="bg-white dark:bg-dark-surface text-black dark:text-white">
-      <div class="container mx-auto">
-        <div class="w-full flex flex-wrap">
-          <div class="w-full md:w-1/3 flex flex-col mb-12 md:mb-0">
-            <div
-              :key="`home-service-web-development-title`"
-              class="service-switch text-2xl font-bold leading-tight pl-4 py-1 my-3 mr-4 cursor-pointer"
-            >
-              <span class="relative z-10">{{ service.title }}</span>
-            </div>
-          </div>
-
-          <div class="w-full md:w-2/3 md:pl-4">
-            <article :key="`home-service-web-development`">
-              <!-- eslint-disable vue/no-v-html -->
-              <service-description
-                v-html="$md.render(service.home_description)"
-              />
-              <!-- eslint-enable -->
-            </article>
-          </div>
-        </div>
-      </div>
-    </section>
+    <page-service class="py-16" :target-services="targetServices" />
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
 export default {
-  props: {
-    targetServices: {
-      type: Array,
-      default: () => []
-    }
+  asyncData({ app, error }) {
+    return Promise.all([import(`@/content/about/${app.i18n.locale}.md`)]).then(
+      ([{ default: about }]) => ({
+        about
+      })
+    )
   },
   data: () => ({
-    activeServiceSlug: null,
     targetServices: ['web-development', 'design']
   }),
-  computed: {
-    activeService() {
-      return this.activeServiceSlug &&
-        Array.isArray(this.sortedServices) &&
-        this.sortedServices.length
-        ? this.sortedServices.filter(
-            ({ slug }) => slug === this.activeServiceSlug
-          )[0]
-        : null
-    },
-    sortedServices() {
-      return this.services
-        .filter(({ slug }) => this.targetServices.includes(slug))
-        .sort(
-          ({ slug: a }, { slug: b }) =>
-            this.targetServices.indexOf(a) - this.targetServices.indexOf(b)
-        )
-    },
-    ...mapState({
-      services: (state) => state.services.records
-    })
-  },
-  created() {
-    this.activeServiceSlug =
-      Array.isArray(this.sortedServices) && this.sortedServices.length
-        ? this.sortedServices[0].slug
-        : null
+  head() {
+    return {
+      title: `${this.$t('services.meta.title')} - ${this.$t(
+        'homepage.meta.shortName'
+      )}`
+    }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.service-switch {
-  position: relative;
-
-  transition-duration: 380ms;
-
-  @media (prefers-reduced-motion: reduce) {
-    transition: none;
-  }
-
-  &::before {
-    @apply bg-secondary-base;
-    content: ' ';
-    position: absolute;
-
-    width: 100%;
-    height: 100%;
-
-    top: 0;
-    left: 0;
-
-    z-index: 5;
-
-    will-change: transform;
-    transition-duration: 380ms;
-
-    transform-origin: bottom left;
-    transform: scaleY(0);
-
-    @media (prefers-reduced-motion: reduce) {
-      transition: none;
-    }
-  }
-
-  .dark-mode & {
-    will-change: color;
-  }
-
-  &:hover,
-  &--active {
-    .dark-mode & {
-      @apply text-black;
-    }
-
-    &::before {
-      transform: scaleY(1);
-    }
-  }
-}
-</style>
